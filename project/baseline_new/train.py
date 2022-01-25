893ebeb1a34b1097f98adba0c98848e3f143929c
unknown <choihj94@gmail.com>
2022-01-25 22:17:32 +0900

    add new baseline code

--- project/baseline_new/train.py
+++ project/baseline_new/train.py
@@ -6,6 +6,8 @@ Baseline for train
 
 import argparse
 import os
+from datetime import datetime
+from typing import Tuple, Dict, Any
 
 import torch
 import torch.nn as nn
@@ -14,6 +16,15 @@ import torch.optim as optim
 import yaml
 
 from src.utils.common import read_yaml
+
+def train(
+    model_config: Dict[str, Any],
+    data_config: Dict[str, Any],
+    log_dir: str,
+    fp16:bool,
+    device:torch.device
+) -> Tuple[float, float, float]:
+    pass
 
 if __name__ == "__main__":
     
@@ -37,4 +48,17 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     log_dir = os.environ.get("SM_MODEL_DIR", os.path.join("exp", "latest"))
 
+    if os.path.exists(log_dir):
+        modified = datetime.fromtimestamp(os.path.getmtime(log_dir + '/best.pt'))
+        new_log_dir = os.path.dirname(log_dir) + '/' + modified.strftime("%Y-%m-%d_%H-%M-%s")
+        os.rename(log_dir, new_log_dir)
+    
+    os.makedirs(log_dir, exist_ok=True)
 
+    test_loss, test_f1, test_acc = train(
+        model_config=model_config,
+        data_config=data_config,
+        log_dir=log_dir,
+        fp16=data_config["FP16"],
+        device=device
+    )
