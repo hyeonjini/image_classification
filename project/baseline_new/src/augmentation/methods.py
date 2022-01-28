66bbc2bba63c12c00858095aab186a5bbd341e82
DESKTOP-E7EPOP3\hyeonjin <choihj94@gmail.com>
2022-01-28 22:19:51 +0900

    [FEAT] add some method for augmentation

---
+++ project/baseline_new/src/augmentation/methods.py
@@ -0,0 +1,26 @@
+"""PyTorch transforms"""
+
+import torchvision.transforms as transforms
+
+DATASET_NORMALIZE_INFO = {
+    "CIFAR10": {"MEAN": (0.4914, 0.4822, 0.4465), "STD": (0.2470, 0.2435, 0.2616)},
+    "CIFAR100": {"MEAN": (0.5071, 0.4865, 0.4409), "STD": (0.2673, 0.2564, 0.2762)},
+    "IMAGENET":{},
+    "MNIST":{},
+    "FashionMNIST":{}
+}
+
+def simple_augment_train(
+    dataset: str,
+    img_size: float = 32
+) -> transforms.Compose:
+    return transforms.Compose([
+        transforms.Resize((int(img_size * 1.2), int(img_size * 1.2))),
+        transforms.RandomResizedCrop(size=img_size, ratio=(0.75, 1.0, 1.25)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            DATASET_NORMALIZE_INFO[dataset]["MEAN"],
+            DATASET_NORMALIZE_INFO[dataset]["STD"],   
+        )
+    ])
